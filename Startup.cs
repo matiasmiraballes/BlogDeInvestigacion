5f48322ac3d111556a80f9188164921afd8f3ffd
Matias Miraballes <matiasmiraballes@hotmail.com>
2019-07-25 22:50:31 -0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogDeInvestigacion.Startup))]
+namespace BlogDeInvestigacion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
