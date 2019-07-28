3441e0008d0d8a2c0c62368889be5b25f1e88b63
Matias Miraballes <matiasmiraballes@hotmail.com>
2019-07-27 23:41:28 -0300

    Agregados roles y usuario admin por defecto

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using BlogDeInvestigacion.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(BlogDeInvestigacion.Startup))]
@@ -9,6 +12,57 @@ namespace BlogDeInvestigacion
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesandUsers();
+        }
+
+
+        // Mas informacion en: https://code.msdn.microsoft.com/ASPNET-MVC-5-Security-And-44cbdb97
+
+        private void CreateRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            // Si aun no existe el rol de Administrador, lo creamos junto con un usuario Admin por defecto
+            if (!roleManager.RoleExists("Administrador"))
+            {
+                // Primero creamos el rol Administrador 
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Administrador";
+                roleManager.Create(role);
+
+                // Luego el usuario
+                var user = new ApplicationUser();
+                user.UserName = "admin@mail.com";
+                user.Email = "admin@mail.com";
+
+                string userPWD = "123456";
+
+                var chkUser = UserManager.Create(user, userPWD);
+  
+                if (chkUser.Succeeded)
+                {
+                    UserManager.AddToRole(user.Id, "Administrador");
+                }
+            }
+
+            // Verificamos que los demas roles se encuentran creados
+            if (!roleManager.RoleExists("Profesor"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Profesor";
+                roleManager.Create(role);
+            }
+
+            // creating Creating Employee role    
+            if (!roleManager.RoleExists("Alumno"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Alumno";
+                roleManager.Create(role);
+            }
         }
     }
 }
