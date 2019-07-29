aed68c1b4cd17e6e752ea6e1e68f54f03b5f80af
Matias Miraballes <matiasmiraballes@hotmail.com>
2019-07-28 23:06:11 -0300

    Agregados controlador y vista "Noticia"

---
+++ Controllers/NoticiaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogDeInvestigacion.Controllers
+{
+    public class NoticiaController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
