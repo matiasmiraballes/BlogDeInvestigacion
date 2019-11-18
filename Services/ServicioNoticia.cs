fdb2b359173fc4e055de6ea29d6e4412a00290e8
Nicolás Palomeque <nico-palomeque@hotmail.com>
2019-11-18 18:56:07 -0300

    Noticias crear,index

---
+++ Services/ServicioNoticia.cs
@@ -0,0 +1,22 @@
+﻿using BlogDeInvestigacion.Data_Management;
+using BlogDeInvestigacion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogDeInvestigacion.Services
+{
+    public class ServicioNoticia
+    {
+        public void GuardarNoticia(Noticia noticia)
+        {
+
+            using (BlogContext db = new BlogContext())
+            {
+                db.Noticias.Add(noticia);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ViewModels/NoticiaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BlogDeInvestigacion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogDeInvestigacion.ViewModels
+{
+    public class NoticiaViewModel
+    {
+        public Noticia Noticia { get; set; }
+        public IList<Laboratorio> Laboratorios { get; set; }
+    }
+}
